5d69f2f4903e1d9e9e4eb904811e2933a429fd29
Snehangshu Biswas <dev.snehangshu@gmail.com>
2023-09-08 11:50:09 +0530

    added jwt utils

---
+++ src/utils/jwt.util.js
@@ -0,0 +1,39 @@
+import jsonwebtoken from 'jsonwebtoken'
+
+import config from '../config/index.js'
+import logger from '../common/logger.js'
+
+const SECRET = config.get('jwtSecret')
+
+/**
+ * Signs and generates a jwt
+ * @param {*} payload data for the jwt payload
+ * @returns jwt token
+ */
+export const signJwt = payload => {
+	try {
+		const token = jsonwebtoken.sign(payload, SECRET, {
+			expiresIn: '10m'
+		})
+
+		return token
+	} catch (error) {
+		logger.error(error, 'failed to sign JWT')
+		return null
+	}
+}
+
+/**
+ * Verifies jwt
+ * @param {*} token string jwt token
+ * @returns the payload if verification is successful
+ */
+export const verifyJwt = token => {
+	try {
+		const payload = jsonwebtoken.verify(token, SECRET)
+		return payload
+	} catch (error) {
+		logger.error(error, 'JWT verification failed')
+		return null
+	}
+}
