f2ba7fcc913f6e1646d63480652c969567667c51
Snehangshu Biswas <dev.snehangshu@gmail.com>
2023-09-08 12:44:00 +0530

    added doctor login controller

--- src/controllers/doctors.controller.js
+++ src/controllers/doctors.controller.js
@@ -2,10 +2,11 @@ import * as argon from 'argon2'
 import validator from 'validator'
 
 import Doctor from '../models/doctor.model.js'
+import { signJwt } from '../utils/jwt.util.js'
 import logger from '../common/logger.js'
 
 /**
- * handles user registration
+ * handles doctor registration
  * @param {*} req express request object
  * @param {*} res express response object
  */
@@ -82,6 +83,83 @@ export const registerDoctorHandler = async (req, res) => {
 		// if something unexpected occurs send a 500 with a failed message
 		return res.status(500).json({
 			message: 'failed to register, please try again'
+		})
+	}
+}
+
+/**
+ * handles doctor login
+ * @param {*} req express request object
+ * @param {*} res express response object
+ */
+export const loginDoctorHandler = async (req, res) => {
+	try {
+		// validate incoming request body
+		if (
+			!req.body.email ||
+			req.body.email === '' ||
+			!validator.isEmail(req.body.email) ||
+			!req.body.password ||
+			req.body.password === ''
+		) {
+			return res.status(400).json({
+				message: 'invalid inputs',
+				errors: {
+					email:
+						!req.body.email || req.body.email === ''
+							? 'email is a required field & cannot be empty'
+							: !validator.isEmail(req.body.email)
+							? 'email has to be a valid email'
+							: undefined,
+					password:
+						!req.body.password || req.body.password === ''
+							? 'password is a required field & cannot be empty'
+							: undefined
+				}
+			})
+		}
+
+		const doctorFound = await Doctor.findOne({ email: req.body.email })
+
+		// if no doctor is found throw a 403
+		if (!doctorFound) {
+			return res.status(403).json({
+				message: 'failed to verify identity'
+			})
+		}
+
+		const passwordMatches = await argon.verify(
+			doctorFound.password,
+			req.body.password
+		)
+
+		if (!passwordMatches) {
+			return res.status(403).json({
+				message: 'failed to verify identity'
+			})
+		}
+
+		const jwtPayload = {
+			id: doctorFound._id.toString()
+		}
+
+		const token = signJwt(jwtPayload)
+
+		if (!token) {
+			return res.status(500).json({
+				message: 'failed to login'
+			})
+		}
+
+		res.status(200).json({
+			message: 'login successful!',
+			access_token: token
+		})
+	} catch (error) {
+		logger.error(error, 'failed to login user')
+		// if something unexpected occurs send a 500 with a failed message
+		return res.status(500).json({
+			message: 'failed to login, please try again'
 		})
 	}
 }
--- src/routes/doctors.route.js
+++ src/routes/doctors.route.js
@@ -1,13 +1,14 @@
 import express from 'express'
 
-import { registerDoctorHandler } from '../controllers/doctors.controller.js'
+import {
+	registerDoctorHandler,
+	loginDoctorHandler
+} from '../controllers/doctors.controller.js'
 
 const doctorsRoute = express.Router()
 
 doctorsRoute.post('/register', registerDoctorHandler)
 
-doctorsRoute.post('/login', (req, res) => {
-	// todo
-})
+doctorsRoute.post('/login', loginDoctorHandler)
 
 export default doctorsRoute
