ccd59d37554709d84aabc24ab0b69ec9ec409cf3
Snehangshu Biswas <dev.snehangshu@gmail.com>
2023-09-08 21:18:06 +0530

    added better validation for patients controller

--- src/controllers/patients.controller.js
+++ src/controllers/patients.controller.js
@@ -1,4 +1,9 @@
 import Patient from '../models/patient.model.js'
+import validator from '../common/validator.js'
+import {
+	RegisterPatientSchema,
+	RegisterPatientResponseSchema
+} from '../validators/patients.validator.js'
 
 /**
  * handles patient registration
@@ -9,46 +14,35 @@ export const registerPatientHandler = async (req, res) => {
 	console.log(req.user)
 	try {
 		// validate incoming request
-		if (
-			!req.body.patientName ||
-			req.body.patientName === '' ||
-			!req.body.phoneNumber ||
-			req.body.phoneNumber === '' ||
-			(req.body.phoneNumber + '').length !== 10
-		) {
+		const [result, errors] = await validator(RegisterPatientSchema, req.body)
+
+		// if validation has errors send a 400
+		if (errors) {
 			return res.status(400).json({
 				message: 'invalid inputs',
-				errors: {
-					patientName:
-						!req.body.patientName || req.body.patientName === ''
-							? 'phoneNumber is a required field & cannot be empty'
-							: undefined,
-					phoneNumber:
-						!req.body.phoneNumber || req.body.phoneNumber === ''
-							? 'phoneNumber is a required field & cannot be empty'
-							: ((req.body.phoneNumber + '').length !== 10) !== 10
-							? 'phone number must be 10 digits'
-							: undefined
-				}
+				errors
 			})
 		}
 
 		const foundPatient = await Patient.findOne({
-			phoneNumber: req.body.phoneNumber
+			phoneNumber: result.phoneNumber
 		})
 
 		if (foundPatient) {
 			return res.status(200).json(foundPatient)
 		}
 
-		const newPatientPayload = {
-			name: req.body.patientName,
-			phoneNumber: req.body.phoneNumber,
-			doctor: req.user._id
-		}
+		// attach doctor to the parsed body
+		result.doctor = req.user._id
 
-		const savedPatient = await Patient.create(newPatientPayload)
-		return res.status(200).json(savedPatient)
+		const savedPatient = await Patient.create(result)
+
+		const [patient] = await validator(
+			RegisterPatientResponseSchema,
+			savedPatient
+		)
+
+		return res.status(200).json(patient)
 	} catch (error) {
 		logger.error(error, 'failed to register patient')
 		// if something unexpected occurs send a 500 with a failed message
---
+++ src/validators/patients.validator.js
@@ -0,0 +1,36 @@
+import z, { object, string } from 'zod'
+import mongoose from 'mongoose'
+
+export const RegisterPatientResponseSchema = object({
+	_id: z.instanceof(mongoose.Types.ObjectId).transform(val => val.toString()),
+	name: string({
+		required_error: 'name is a required field'
+	}).nonempty('name cannot be empty'),
+	phoneNumber: string({
+		required_error: 'phoneNumber is a required field'
+	})
+		.length(10, 'phone number must be of 10 digits')
+		.regex(/^[0-9]+$/, 'phone number must contain only numbers'),
+	createdAt: string({ required_error: 'createdAt is a required field' })
+}).transform(patient => {
+	return {
+		id: patient._id.toString(),
+		name: patient.name,
+		phoneNumber: patient.phoneNumber,
+		createdAt: patient.createdAt
+	}
+})
+
+/**
+ * Validation schema for patient registration
+ */
+export const RegisterPatientSchema = object({
+	name: string({
+		required_error: 'name is a required field'
+	}).nonempty('name cannot be empty'),
+	phoneNumber: string({
+		required_error: 'phoneNumber is a required field'
+	})
+		.length(10, 'phone number must be of 10 digits')
+		.regex(/^[0-9]+$/, 'phone number must contain only numbers')
+})
